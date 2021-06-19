2e979f517bb6c36595b7d03f7bd1e7ec0a856b0d
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-06-19 13:50:45 -0500

    create ch6ex2

---
+++ chapter6.ex02grades.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+  float score;
+
+  printf("Enter the score of the student between 0 and 100.\n");
+  scanf("%f",&score);
+
+  if ((score>=0)&&(score<=60))
+    printf("You got an F, study harder!\n");
+  if ((score>=60)&&(score<=70))
+    printf("You got an E, keep working!\n");
+  if ((score>=70)&&(score<=80))
+    printf("You got a C, you can do better.\n");  
+  if ((score>=80)&&(score<=90))
+    printf("You got a B, you are doing great!\n");
+  if ((score>=90)&&(score<=100))
+    printf("You got an A, congratulations!\n");
+  if ((score>100)||(score<0))
+    printf("You must enter a valid score between 0 and 100.");
+
+  return 0;
+}
