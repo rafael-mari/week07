812a12ab5496c94a9950212ae34185d7304afeda
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-06-23 09:55:57 -0500

    Update chapter6.ex05leapyear.c

--- chapter6.ex05leapyear.c
+++ chapter6.ex05leapyear.c
@@ -2,10 +2,12 @@
 #include <math.h> 
 
 int main(void) {
-  int a ;
+  int a;
+  char bvb[100];
 
   printf("Enter a year to know if it is a leap-year\n");
-  scanf("%d",&a);
+  fgets(bvb, sizeof(bvb), stdin);
+  sscanf(bvb, "%d", &a);
     if (a % 400 == 0){
         printf ("%d is a leap year.\n",a);
     }
