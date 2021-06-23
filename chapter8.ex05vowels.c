54b3d3c9459049625b2c3abb436ed853fa6eb79b
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-06-23 10:58:25 -0500

    Update chapter8.ex05vowels.c

--- chapter8.ex05vowels.c
+++ chapter8.ex05vowels.c
@@ -17,12 +17,14 @@
 int main(void) {
   int lt;//letter
   int a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z;
+  char bvb[100]; //To read up to 100 characters the user enters
 
   printf("a=1    b=2    c=3    d=4    e=5    f=6    g=7 \nh=8    i=9    j=10   k=11   l=12   m=13   n=14 \nñ=15   o=16   p=17   q=18   r=19   s=20   t=21 \nu=22   v=23   w=24   x=25   y=26   z=27\n");
 
 
   printf("Enter the number of the letter you want to know whether it is a vowel or a consonant.\n");
-  scanf("%d",&lt);
+  fgets(bvb, sizeof(bvb), stdin);
+  sscanf(bvb, "%d",&lt);
   
   a=1, b=2, c=3, d=4, e=5, f=6, g=7, h=8, i=9, j=10, k=11, l=12, m=13, n=14, ñ=15, o=16, p=17, q=18, r=19, s=20, t=21, u=22, v=23, w=24, x=25, y=26, z=27;
 
