348aca583dd2d0e998270699e0e8ec631fe83196
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-06-23 09:53:14 -0500

    Update chapter6.ex04cents.c

--- chapter6.ex04cents.c
+++ chapter6.ex04cents.c
@@ -7,16 +7,23 @@ int main(void){
   int dim;
   int nck;
   int pen;
+  char bvb[100];
 
   printf("Enter the amount of money in cents.\n");
-  scanf("%d", &mon);
+  fgets(bvb, sizeof(bvb), stdin);
+  sscanf(bvb, "%d", &mon);
 
-  qrt=trunc(mon/25);
-  dim=trunc((mon-qrt*25)/10);
-  nck=trunc((mon-qrt*25-dim*10)/5);
-  pen=trunc(mon-qrt*25-dim*10-nck*5);
+  if ((mon>=0)&&(mon<=100)){
+    qrt=trunc(mon/25);
+    dim=trunc((mon-qrt*25)/10);
+    nck=trunc((mon-qrt*25-dim*10)/5);
+    pen=trunc(mon-qrt*25-dim*10-nck*5);
 
-  printf("You need %d quarter(s), %d dime(s), %d nickle(s) and %d penny(ies).",qrt,dim,nck,pen);
+    printf("You need %d quarter(s), %d dime(s), %d nickle(s) and %d penny(ies).",qrt,dim,nck,pen);
+  }
+  else{
+    printf("Enter the amount between 0 and 100 cents.");
+  }
 
   return 0;
 }
